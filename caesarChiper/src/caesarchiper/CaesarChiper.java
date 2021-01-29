48aefb4b6c569531e3034844fbbdda3e49833c8f
Flexedosp <farinoer13@gmail.com>
2021-01-29 22:51:55 +0700

    Repaired

---
+++ caesarChiper/src/caesarchiper/CaesarChiper.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caesarchiper;
+
+/**
+ *
+ * @author Alghifari
+ */
+public class CaesarChiper {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    // Encrypts text using a shift od s 
+    public static StringBuffer encrypt(String text, int s) 
+    { 
+        StringBuffer result= new StringBuffer(); 
+  
+        for (int i=0; i<text.length(); i++) 
+        { 
+            if (Character.isUpperCase(text.charAt(i))) 
+            { 
+                char ch = (char)(((int)text.charAt(i) + 
+                                  s - 65) % 26 + 65); 
+                result.append(ch); 
+            } 
+            else
+            { 
+                char ch = (char)(((int)text.charAt(i) + 
+                                  s - 97) % 26 + 97); 
+                result.append(ch); 
+            } 
+        } 
+        return result; 
+    } 
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String text = "ATTACKATONCE"; 
+        int s = 4; 
+        System.out.println("Text  : " + text); 
+        System.out.println("Shift : " + s); 
+        System.out.println("Cipher: " + encrypt(text, s));
+    }
+    
+}
